325d359bd7055d5b6dfb3cef53f85fe1bb68281b
bdrummo6 <bradley.drummonds@gmail.com>
2020-04-13 23:03:21 -0400

    Completed Step #2 of the project
    Displayed my user data to the DOM.

--- react-github-usercard/src/App.js
+++ react-github-usercard/src/App.js
@@ -34,7 +34,20 @@ class App extends Component {
       return (
          <div className='App'>
             <h1>Github UserCard</h1>
-
+            <div className='usercard'>
+               <h3>{userState.name}</h3>
+               <div className='avatar'>
+                  <img src={userState.avatar_url} alt={userState.avatar_url} key={userState.avatar_url} />
+               </div>
+               <div className='userData'>
+                  <p><span>Username:</span> {userState.login}</p>
+                  <p><span>ID:</span> {userState.id}</p>
+                  <p><span>URL:</span> <a href={userState.html_url}>{userState.html_url}</a></p>
+                  <p><span>Repos:</span> {userState.public_repos}</p>
+                  <p><span>Followers:</span> {userState.followers}</p>
+                  <p><span>Following:</span> {userState.following}</p>
+               </div>
+            </div>
          </div>
       )
    }
