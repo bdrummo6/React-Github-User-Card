7fd0eed593c0de3800cf26a494518b3872e775a0
bdrummo6 <bradley.drummonds@gmail.com>
2020-04-13 23:00:39 -0400

    Completed Step #1 of Project
    I fetched my github user data from the github API.

--- react-github-usercard/src/App.js
+++ react-github-usercard/src/App.js
@@ -4,14 +4,40 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-       <div className='App'>
+   constructor() {
+      super();
+      this.state = {
+         users: [],
+      };
 
-       </div>
-    );
-  }
+   }
 
+   componentDidMount() {
+      // console.log('Component did mount!');
+
+      fetch(`https://api.github.com/users/bdrummo6`)
+         .then(results => results.json())
+         .then(users => {
+            // console.log('users: ', users);
+            this.setState({ users: users });
+         })
+         .catch(err => {
+            console.log('Error: ', err)
+         });
+   }
+
+
+   render() {
+
+      let userState = this.state.users;
+
+      return (
+         <div className='App'>
+            <h1>Github UserCard</h1>
+
+         </div>
+      )
+   }
 }
 
 export default App;
